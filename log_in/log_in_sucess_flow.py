98c51a0f617a9d8aef04ca8a8a671c8dc98eceae
laksana <laksana@kkumail.com>
2025-09-15 11:41:59 +0700

    update test

---
+++ log_in/log_in_sucess_flow.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+# test for admin
+
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+
+driver.get("https://shrinkagemanagementuat.tops.co.th/")
+time.sleep(6)  
+
+username_input = driver.find_element(By.CSS_SELECTOR, 'textarea[data-semantics-role="text-field"]')
+username_input.send_keys("anlaksana@central.co.th")
+password_input = driver.find_element(By.CSS_SELECTOR, "input[aria-label='กรุณากรอกรหัสผ่าน']")
+password_input.send_keys("Password2025")
+time.sleep(6) 
+
+login_button = driver.find_element(By.XPATH, "//*[contains(text(), 'เข้าสู่ระบบ')]")
+login_button.click()
+time.sleep(6)  
+
+
+driver.quit()
+
